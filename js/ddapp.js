3e5ad4590401fdc2344f7c226deace9770b7f8de
theMightyT <halvas_trevor@halvas.ca>
2021-06-29 14:37:33 -0400

    adding image swap

--- js/ddapp.js
+++ js/ddapp.js
@@ -1,34 +1,55 @@
+// wrap everything in an IIFE / module
+// a module is a JavaScript "pattern" - a programming convention
+// this keeps your code private - kinda like a "black box" - which is a best practice
+
+(() => {
     //identify the nodes of interest in the DOM
-const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
+	const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
 				dropcontainer = document.querySelector(".puzzle-board"),
 				dragimages = document.querySelectorAll(".puzzle-image"),
 				dropzones = document.querySelectorAll(".drop-zone");
 
-	// functions go in the middle
+		// functions go in the middle
+		function swapImages() {
+			// swap out the draggable thumbnail images
+			// update the backgound image of the drop zone dropcontainer
+			// 1. get the imageref attribute from the element we're clicking on
 
-	function startDrag() {
-		console.log('dragging ' + this.dataset.piecenum);
-	}
+			// let imageIndex = this.dataset.imageref,
+			// 		// newImagePath = "images/dd/backGround" + imageIndex,
+			// 		newImagePath = `url(images/dd/backGround${imageIndex}.jpg)`;
 
-	function draggedOver(event) {
-		event.preventDefault();
-		console.log('dragging over drop zone elements');
-	}
+			// 2. set the backround image of the dropcontainer
+			dropcontainer.style.backgroundImage = `url(images/dd/backGround${this.dataset.imageref}.jpg)`;
+			// debugger;
+		}
 
-	function dropped(event) {
-		event.preventDefault();
-		console.log('dropped on the element');
-		console.log(event.target.id);
-	}
+		function startDrag() {
+			console.log('dragging ' + this.dataset.piecenum);
+		}
+
+		function draggedOver(event) {
+			event.preventDefault();
+			console.log('dragging over drop zone elements');
+		}
+
+		function dropped(event) {
+			event.preventDefault();
+			console.log('dropped on the element');
+			console.log(event.target.id);
+		}
 
 
-	// event handling at the bottom
-dragimages.forEach(piece => {
-	piece.addEventListener('dragstart', startDrag, false);
-});
+		// event handling at the bottom
+	dragimages.forEach(piece => {
+		piece.addEventListener('dragstart', startDrag, false);
+	});
 
 
-dropzones.forEach(zone => {
-	zone.addEventListener('drop',dropped, false);
-	zone.addEventListener('dragover',draggedOver, false);
-});
+	dropzones.forEach(zone => {
+		zone.addEventListener('drop',dropped, false);
+		zone.addEventListener('dragover',draggedOver, false);
+	});
+
+	puzzleSelectors.forEach(button => button.addEventListener("click", swapImages));
+})();
